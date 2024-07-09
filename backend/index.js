c80fd2dc1194245f84bbf43133caf468e72cd49f
chinnasivakrishna <139040548+chinnasivakrishna@users.noreply.github.com>
2024-07-09 10:46:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,7 @@ app.use(function (req, res, next) {
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
       next();
     });
 const DB_URL = process.env.MONGO_URL;
